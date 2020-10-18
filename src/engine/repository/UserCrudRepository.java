7585875c564ad230ace52965caa365cff9d51013
Valery Kravtsov <vijit@mail.ru>
2020-10-19 01:23:18 +0700

    Use Optional result of finding User.

--- src/engine/repository/UserCrudRepository.java
+++ src/engine/repository/UserCrudRepository.java
@@ -3,6 +3,8 @@ package engine.repository;
 import engine.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserCrudRepository extends CrudRepository<User, Integer> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
--- src/engine/service/UserService.java
+++ src/engine/service/UserService.java
@@ -3,6 +3,7 @@ package engine.service;
 import engine.dto.UserDetailsDto;
 import engine.dto.UserDto;
 import engine.entity.User;
+import engine.exception.EntityNotFoundException;
 import engine.exception.NotUniqueValueException;
 import engine.repository.UserCrudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,8 +28,8 @@ public class UserService implements UserDetailsService {
      */
     public void addUser(UserDto user) throws NotUniqueValueException {
         String email = user.getEmail();
-        if (isEmailExist(email)) {
-            throw new NotUniqueValueException("Email " + email  + " occupied already");
+        if (repo.findByEmail(email).isPresent()) {
+            throw new NotUniqueValueException("Email " + email + " occupied already");
         }
 
         User userEntity = new User()
@@ -39,22 +40,15 @@ public class UserService implements UserDetailsService {
         repo.save(userEntity);
     }
 
-    private boolean isEmailExist(String email) {
-        User user = repo.findByEmail(email);
-        return user != null;
-    }
-
     /**
      * <a href="https://habr.com/ru/company/otus/blog/488418/">Аутентификация REST API с помощью Spring Security...</a>
      * <a href="https://www.youtube.com/watch?v=TNt3GHuayXs">JetBrains: Spring Security + MySQL</a>
      */
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = repo.findByEmail(email);
-
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found by " + email);
-        }
+        User user = this.repo
+            .findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found by " + email));
 
         return new UserDetailsDto(user);
     }
